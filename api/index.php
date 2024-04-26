5249dc407611f52b127e6ca92602c8f5e0c51887
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-04-27 05:37:11 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,7 @@
+<?php
 
-require 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$ffmpeg = FFMpeg\FFMpeg::create();
-$video = $ffmpeg->open('video.mpg');
-$video
-    ->filters()
-    ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
-    ->synchronize();
-$video
-    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
-    ->save('frame.jpg');
-$video
-    ->save(new FFMpeg\Format\Video\X264(), 'export-x264.mp4')
-    ->save(new FFMpeg\Format\Video\WMV(), 'export-wmv.wmv')
-    ->save(new FFMpeg\Format\Video\WebM(), 'export-webm.webm');
+Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
+
+throw new RuntimeException('Hello Tracy!');
