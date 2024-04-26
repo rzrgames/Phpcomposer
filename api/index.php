a3a8b557a31dd8a977cdcefe6cae0d0ac2615592
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-04-27 05:00:50 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,16 @@
 
+require 'vendor/autoload.php';
+
+$ffmpeg = FFMpeg\FFMpeg::create();
+$video = $ffmpeg->open('video.mpg');
+$video
+    ->filters()
+    ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
+    ->synchronize();
+$video
+    ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
+    ->save('frame.jpg');
+$video
+    ->save(new FFMpeg\Format\Video\X264(), 'export-x264.mp4')
+    ->save(new FFMpeg\Format\Video\WMV(), 'export-wmv.wmv')
+    ->save(new FFMpeg\Format\Video\WebM(), 'export-webm.webm');
